b0f78475476f327a6b0f99e2358ac917cabfd647
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-03-24 10:34:07 +0200

    initial commit

--- database/migrations/2025_03_24_083207_create_repair_orders_table.php
+++ database/migrations/2025_03_24_083207_create_repair_orders_table.php
@@ -13,7 +13,19 @@ return new class extends Migration
     {
         Schema::create('repair_orders', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestamp('stamp'); // Carimbo de Data e Hora
+            $table->string('repair_order'); // Ordem de Reparação
+            $table->string('machine_number'); // Número da Máquina
+            $table->string('month_entry', 3); // Mês de Entrada (ex.: JAN, FEV)
+            $table->integer('year_entry'); // Ano
+            $table->date('date_entry'); // Data de Entrada
+            $table->text('breakdown_description'); // Descrição da Avaria
+            $table->string('mantainance_type'); // Tipo de Manutenção
+            $table->string('applicant'); // Solicitante
+            $table->string('work_state'); // Estado da Obra
+            $table->string('location'); // Localização da Obra
+            $table->string('cliente'); // Cliente
+            $table->timestamps(); // created_at e updated_at
         });
     }
 
